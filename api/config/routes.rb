4766284f6035867d8b4d2d92c97975bdc54b2043
中村昌克 <nakamura.masakatsu@gmail.com>
2023-08-13 16:47:13 +0900

    add users model

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # 追加
   namespace :api do
     namespace :v1 do
-      # api test action
-      resources :hello, only:[:index]
+      resources :users, only:[:index]
     end
   end
 end
